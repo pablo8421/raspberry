5872229a143761264692bfd75160ad27a586af12
pablo8421 <pablo_jsd-8@hotmail.com>
2015-05-11 16:48:04 -0600

    asdf

---
+++ array.s
@@ -0,0 +1,154 @@
+.globl main
+main:
+	LDR R11,=_local$
+	LDR R12,=_global$
+	push {lr}
+	LDR R0 ,= _retorno
+	STR lr , [R0]
+	MOV R0, #0
+	push {R11}
+	BL main$
+	pop {R4}
+	BX R4
+
+
+main$:
+
+	pop {R11}
+	push {lr}
+	MOV R4 , #7
+	STR R4, [R11, #4]
+	MOV R4 , #4
+	MOV R5 , R4
+	ADD R6, #4, #24
+	ADD R7, R6, #8
+	MUL R6, R4, #8
+	ADD R4, R7, R6
+	CMP 0,R5
+	MOVLT R1, R5
+	BLT ERROR_INDEX_OUT_OF_BOUNDS$
+	CMP R5,5
+	MOVGE R1, R5
+	BGE ERROR_INDEX_OUT_OF_BOUNDS$
+	MOV R5 , #4
+	STR R5, [R11, R4]
+	push {R11}
+	MOV R4 , #4
+	MOV R5 , R4
+	ADD R6, #4, #24
+	ADD R7, R6, #8
+	MUL R6, R4, #8
+	ADD R4, R7, R6
+	CMP 0,R5
+	MOVLT R1, R5
+	BLT ERROR_INDEX_OUT_OF_BOUNDS$
+	CMP R5,5
+	MOVGE R1, R5
+	BGE ERROR_INDEX_OUT_OF_BOUNDS$
+	LDR R5, [R11, R4]
+	push {R5}
+	push {R5}
+	ADD R11, R11, #188
+	push {R11}
+	BL imprimirInt_int$
+	pop {R4}
+	pop {R11}
+	pop {pc}
+
+obtenerChar$:
+	pop {R11}
+	push {lr}
+	BL getchar
+	pop {R3}
+	push {R0}
+	LDR R12,=_global$
+	MOV pc, R3
+
+obtenerInt$:
+	pop {R11}
+	push {lr}
+	LDR R0,=_scanFormato
+	LDR R1,=_scanLugar
+	BL scanf
+	CMP R0, #0
+	BEQ ERROR_INGRESO_ERRONEO$
+	LDR R1,=_scanLugar
+	LDR R0, [R1]
+	pop {R3}
+	push {R0}
+	LDR R12,=_global$
+	MOV pc, R3
+
+imprimirChar_char$:
+	pop {R11}
+	pop {R1}
+	push {lr}
+	LDR R0 ,= _char
+	BL printf
+	LDR R12,=_global$
+	pop {pc}
+
+imprimirInt_int$:
+	pop {R11}
+	pop {R1}
+	push {lr}
+	LDR R0 ,= _int
+	BL printf
+	LDR R12,=_global$
+	pop {pc}
+
+imprimirEspacio$:
+	pop {R11}
+	push {lr}
+	LDR R0 ,= _espacio
+	BL printf
+	LDR R12,=_global$
+	pop {pc}
+
+ERROR_INDEX_OUT_OF_BOUNDS$:
+
+	LDR R0 ,= _index_fuera
+	bl printf
+	LDR R1 ,= _retorno
+	LDR R0 , [R1]
+	BX R0
+
+ERROR_INGRESO_ERRONEO$:
+
+	LDR R0 ,= _ingreso_erroneo
+	bl printf
+	LDR R1 ,= _retorno
+	LDR R0 , [R1]
+	BX R0
+
+.data
+.align 4
+
+_retorno:
+.word
+_index_fuera:
+.asciz "A una lista se le paso el indice %d fuera de su tamanio\n"
+
+_ingreso_erroneo:
+.asciz "No se ingreso un numero en el formato pedido\n"
+
+_int:
+.asciz "%d\n"
+
+_char:
+.asciz "%c"
+
+_espacio:
+.asciz "\n"
+
+_scanFormato:
+.asciz "%d"
+
+_scanLugar:
+.word
+
+_global$:
+.space 2
+
+_local$:
+.space 1024
